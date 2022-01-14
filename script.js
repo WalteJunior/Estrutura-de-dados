e75c2443c3fe5e4372639218a7c61959b24f6722
WalterJunior <walterjuniordossanto@gmail.com>
2022-01-14 20:37:57 -0300

    Estrutura de JS

---
+++ script.js
@@ -0,0 +1,9 @@
+let name = "Walter";
+let number = 10;
+let year = 2022;
+console.log(year - number)
+console.log(year + number)
+console.log("Estou mexendo no console.log")
+let quartafeira = true;
+console.log(quartafeira)
+console.log(typeof quartafeira)
